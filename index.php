9e9f736fb7debb2087a0223c8bf2b7c934ae8b63
enerjhun relon <enerjhunrelon@gmail.com>
2022-10-27 09:29:46 +0800

    a

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <link href="style.css" rel="stylesheet" type=" text/css">
+    <meta charset="UTF-8">
+    <title>Page title</title>
+</head>
+
+<body>
+    <nav id="top_nav">
+        <div id="search_con">
+            <input type="text" placeholder="search">
+        </div>
+        <p>School logo</p>
+        <p class="nav_button">Calendar</p>
+        <p class="nav_button">Message box</p>
+        <p class="nav_button">Menu</p>
+    </nav>
+    <nav id="side_nav">
+        <p>Admin</p>
+        <p>Staff</p>
+        <p>User</p>
+    </nav>
+
+    <div id="recommended_container" class="card_con">
+        <div class="card">1</div>
+
+        <div class="card">2</div>
+
+        <div class="card">3</div>
+
+        <div class="card">4</div>
+
+        <div class="card">5</div>
+
+        <div class="card">6</div>
+
+        <div class="card">7</div>
+
+    </div>
+    <div id="new_container" class="card_con">
+
+        <div class="card">8</div>
+
+        <div class="card">9</div>
+
+        <div class="card">10</div>
+
+        <div class="card">11</div>
+
+        <div class="card">12</div>
+
+        <div class="card">13</div>
+
+        <div class="card">14</div>
+
+    </div>
+</body>
+
+</html>
