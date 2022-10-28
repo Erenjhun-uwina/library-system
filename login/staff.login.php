99fb1e9f8e0816f388bd37ee2c3c971ee4d12115
= <=>
2022-10-28 14:45:26 +0800

    done

--- login/staff.login.php
+++ login/staff.login.php
@@ -3,6 +3,7 @@
 
 <head>
     <link href="../style/admin.login.css" rel="stylesheet" type="text/css">
+    <link href="../style/staff.login.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
--- login/user.login.php
+++ login/user.login.php
@@ -3,6 +3,7 @@
 
 <head>
     <link href="../style/admin.login.css" rel="stylesheet" type="text/css">
+    <link href="../style/user.login.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
