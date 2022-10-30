565660d9d55335b6910f72d57a9a4239f656915f
enerjhun relon <enerjhunrelon@gmail.com>
2022-10-30 20:02:59 +0800

    test

--- test.php
+++ test.php
@@ -1,22 +1,10 @@
 <?php
 
 require_once("./controller/UserCtrl.php");
-require_once("./controller/BookCtrl.php");
 
+$ctrl = new UserCtrl;
 
-
-
-
-
-$Book = new BookCtrl;
-// $Book -> create("The World", "LopezVito","10-10-2000", "Fantasy", "" , "Amazon Inc.", "English" );
-
-
-// $Book -> DELETE ("6");
-
-// $Book -> update("The world ver.2", "Lim", "12-12-2020", "Fantasy"," ", "books.Inc", "English", 7);
 
 
 // echo $ctrl->create("erejhun","chonggus","2003-11-29","erejhun@gmail.com","chongusss","phil");
-
-
+// echo $ctrl->delete(8);
