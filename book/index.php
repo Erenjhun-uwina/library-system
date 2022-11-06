e55c6c3cb086ae0e820db32b352d3082de7fab6f
James-Mhico <114966949+James-Mhico@users.noreply.github.com>
2022-11-06 20:44:58 +0800

    change pass

--- book/index.php
+++ book/index.php
@@ -10,6 +10,24 @@
 
 </head>
 <body>  
- <div></div>   
+ <section id="book_review">
+ <img src="" alt="">
+ <h1>Title</h1>
+ <h2>Details</h2>
+ </section>
+ <section id="change_pass">
+    <form>
+    <input type="text" placeholder="Username" name="user" required><br>
+    <input type="password" placeholder="Password" name="pass" required><br>
+    <input type="password" placeholder="New_Password" name="pass" required>
+    <button id="submit" type="submit">Update</button>
+
+
+
+    </form>
+</section>
+
+
+   
 </body>
 </html>
